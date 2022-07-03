e304cb27a4fb823c98950e3e5ce3ef36f0eb9023
Tudor Murariu <betterthantfblade@yahoo.com>
2022-07-03 18:20:48 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Pacanele
+{
+    partial class Pacanele
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pacanele));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.Titlu = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Credits = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Win = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Bet = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.ALLIN = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Pacanele";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Titlu
+            // 
+            this.Titlu.AutoSize = true;
+            this.Titlu.Font = new System.Drawing.Font("Sitka Small", 26.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Titlu.Location = new System.Drawing.Point(303, 9);
+            this.Titlu.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Titlu.Name = "Titlu";
+            this.Titlu.Size = new System.Drawing.Size(194, 52);
+            this.Titlu.TabIndex = 5;
+            this.Titlu.Text = "Pacanele ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(24, 105);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Credits :";
+            // 
+            // Credits
+            // 
+            this.Credits.AutoSize = true;
+            this.Credits.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Credits.Location = new System.Drawing.Point(139, 105);
+            this.Credits.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Credits.Name = "Credits";
+            this.Credits.Size = new System.Drawing.Size(55, 29);
+            this.Credits.TabIndex = 7;
+            this.Credits.Text = "500";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(244, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Bet :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(528, 105);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 29);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Win :";
+            // 
+            // Win
+            // 
+            this.Win.AutoSize = true;
+            this.Win.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Win.Location = new System.Drawing.Point(633, 105);
+            this.Win.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Win.Name = "Win";
+            this.Win.Size = new System.Drawing.Size(27, 29);
+            this.Win.TabIndex = 11;
+            this.Win.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Location = new System.Drawing.Point(297, 358);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 48);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "S P I N";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Bet
+            // 
+            this.Bet.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Bet.Location = new System.Drawing.Point(335, 105);
+            this.Bet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Bet.Name = "Bet";
+            this.Bet.Size = new System.Drawing.Size(94, 35);
+            this.Bet.TabIndex = 13;
+            this.Bet.Text = "15";
+            this.Bet.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Bet.TextChanged += new System.EventHandler(this.Bet_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(381, 118);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(6, 6);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(11, 11);
+            this.axWindowsMediaPlayer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer1.TabIndex = 15;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // ALLIN
+            // 
+            this.ALLIN.BackColor = System.Drawing.Color.Yellow;
+            this.ALLIN.Font = new System.Drawing.Font("Tempus Sans ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ALLIN.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.ALLIN.Location = new System.Drawing.Point(548, 12);
+            this.ALLIN.Name = "ALLIN";
+            this.ALLIN.Size = new System.Drawing.Size(145, 51);
+            this.ALLIN.TabIndex = 16;
+            this.ALLIN.Text = "Nush";
+            this.ALLIN.UseVisualStyleBackColor = false;
+            this.ALLIN.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(629, 162);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(130, 179);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(474, 162);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(130, 179);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(321, 162);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(130, 179);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(166, 162);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(130, 179);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 162);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 179);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Pacanele
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(767, 412);
+            this.Controls.Add(this.ALLIN);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Bet);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Win);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Credits);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Titlu);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Pacanele";
+            this.Text = "Pacanelele";
+            this.Load += new System.EventHandler(this.Pacanele_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label Titlu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Credits;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Win;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Bet;
+        private System.Windows.Forms.Button button2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button ALLIN;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Pacanele
+{
+    public partial class Pacanele : Form
+    {
+        //WindowsMediaPlayer player = new WindowsMediaPlayer();
+        public Pacanele()
+        {
+            InitializeComponent();
+            //player.controls.
+           /// player.controls.play();
+            // 3 de 7 + *5 bet
+            // 4 de 7 + *7 bet
+            // 5 de 7 + * 20 bet
+            // la 2 nu pierzi nici nu castigi
+            // boaba *2 *3 *7
+            // portocala *3 *4 *9
+            // lamaie *3 *4 *9
+            // pepene *4 *5 *10
+        }
+        //string bet, Credits;
+        int wins = 0, betMoney = 15, creditMoney = 500;
+        Random rnd = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (creditMoney == 0)
+                {
+                    MessageBox.Show("Ti.ai pierdut banii golane", "F , ai avut " + wins.ToString() + " win.uri", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                    Application.Exit();
+                }
+                else if (Convert.ToInt32(Bet.Text) == 0)
+                    MessageBox.Show("Mai Golane", "Vai Vai Vai", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                else if (Convert.ToInt32(Bet.Text) <= Convert.ToInt32(Credits.Text))
+                    Spin();
+                else
+                {
+                    MessageBox.Show("Your bet has to be lower or equal than your credits .\nYou have to lower the Bet !", "!!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("ERROR", "Your bet has to be an number");
+            }
+        }
+
+        private void Pacanele_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            betMoney = creditMoney;
+            this.Bet.Text = betMoney.ToString();
+        }
+
+        /// <summary>
+        /// %7 o sa fie 7
+        /// %3 si nu cu 2 o sa fie portocala
+        /// %3 si la %2 o sa fie lamaie
+        /// %4 pepene
+        /// </summary>
+        void Spin()
+        {    
+            int l1, l2, l3, l4, l5;
+            l1 = rnd.Next(0, 5);
+            l2 = rnd.Next(0, 5);
+            l3 = rnd.Next(0, 5);
+            l4 = rnd.Next(0, 5);
+            l5 = rnd.Next(0, 5);
+            ///
+            //l1 = 0; l2 = 0; l3 = 0;
+            ///
+            // pt l1
+            switch (l1)
+            {
+                case 0:
+                    this.pictureBox1.Image = Properties.Resources.tenor;
+                    break;
+                case 1:
+                    this.pictureBox1.Image = Properties.Resources.Hangan;
+                    break;
+                case 2:
+                    this.pictureBox1.Image = Properties.Resources.SSS;
+                    break;
+                case 3:
+                    this.pictureBox1.Image = Properties.Resources.Oh_;
+                    break;
+                case 4:
+                    this.pictureBox1.Image = Properties.Resources.PFFT;
+                    break;               
+            }
+            // pt l2
+            switch (l2)
+            {
+                case 0:
+                    this.pictureBox2.Image = Properties.Resources.tenor;
+                    break;
+                case 1:
+                    this.pictureBox2.Image = Properties.Resources.Hangan;
+                    break;
+                case 2:
+                    this.pictureBox2.Image = Properties.Resources.SSS;
+                    break;
+                case 3:
+                    this.pictureBox2.Image = Properties.Resources.Oh_;
+                    break;
+                case 4:
+                    this.pictureBox2.Image = Properties.Resources.PFFT;
+                    break;
+            }
+            //pt l3
+            switch (l3)
+            {
+                case 0:
+                    this.pictureBox3.Image = Properties.Resources.tenor;
+                    break;
+                case 1:
+                    this.pictureBox3.Image = Properties.Resources.Hangan;
+                    break;
+                case 2:
+                    this.pictureBox3.Image = Properties.Resources.SSS;
+                    break;
+                case 3:
+                    this.pictureBox3.Image = Properties.Resources.Oh_;
+                    break;
+                case 4:
+                    this.pictureBox3.Image = Properties.Resources.PFFT;
+                    break;
+            }
+            // pt l4
+            switch (l4)
+            {
+                case 0:
+                    this.pictureBox4.Image = Properties.Resources.tenor;
+                    break;
+                case 1:
+                    this.pictureBox4.Image = Properties.Resources.Hangan;
+                    break;
+                case 2:
+                    this.pictureBox4.Image = Properties.Resources.SSS;
+                    break;
+                case 3:
+                    this.pictureBox4.Image = Properties.Resources.Oh_;
+                    break;
+                case 4:
+                    this.pictureBox4.Image = Properties.Resources.PFFT;
+                    break;
+            }
+            // pt l5
+            switch (l5)
+            {
+                case 0:
+                    this.pictureBox5.Image = Properties.Resources.tenor;
+                    break;
+                case 1:
+                    this.pictureBox5.Image = Properties.Resources.Hangan;
+                    break;
+                case 2:
+                    this.pictureBox5.Image = Properties.Resources.SSS;
+                    break;
+                case 3:
+                    this.pictureBox5.Image = Properties.Resources.Oh_;
+                    break;
+                case 4:
+                    this.pictureBox5.Image = Properties.Resources.PFFT;
+                    break;
+            }
+            // 7 7 7 7 7          
+            if (l1 == 1 && l2 == 1 && l3 == 1 && l4 == 1 && l5 == 1)
+            {
+                creditMoney = creditMoney + betMoney * 7;
+                this.Credits.Text = creditMoney.ToString();
+                wins++;
+                Win.Text = wins.ToString();
+            }
+            else if (l1 == l2 && l1 == l3 && l3 == l4 && l1 == l5)
+            {
+                creditMoney = creditMoney + betMoney * 5;
+                this.Credits.Text = creditMoney.ToString();
+                this.wins++;
+                this.Win.Text = wins.ToString();
+            }
+            else if ((l1 == 1 && l2 == 1 && l3 == 1 && l4 == 1) || (l1 == 1 && l2 == 1 && l3 == 1 && l5 == 1) || (l1 == 1 && l2 == 1 && l5 == 1 && l4 == 1) || (l1 == 1 && l5 == 1 && l3 == 1 && l4 == 1) || (l5 == 1 && l2 == 1 && l3 == 1 && l4 == 1))
+            {
+                creditMoney = creditMoney + betMoney * 5;
+                this.Credits.Text = creditMoney.ToString();
+                this.wins++;
+                Win.Text = wins.ToString();
+            }
+            else if ((l1 == l2 && l1 == l3 && l3 == l4) || (l1 == l2 && l1 == l3 && l3 == l5) || (l1 == l2 && l1 == l5 && l5 == l4) || (l1 == l5 && l1 == l3 && l3 == l4) || (l5 == l2 && l5 == l3 && l3 == l4))
+            {
+                creditMoney = creditMoney + betMoney * 3 + 30;
+                this.Credits.Text = creditMoney.ToString();
+                this.wins++;
+                this.Win.Text = wins.ToString();
+            }
+            else if ((l1 == 1 && l2 == 1 && l3 == 1) || (l1 == 1 && l2 == 1 && l4 == 1) || (l1 == 1 && l2 == 1 && l5 == 1) || (l1 == 1 && l4 == 1 && l3 == 1) || (l1 == 1 && l5 == 1 && l3 == 1) || (l4 == 1 && l2 == 1 && l3 == 1) || (l1 == 1 && l4 == 1 && l5 == 1) || (l4 == 1 && l2 == 1 && l5 == 1) || (l4 == 1 && l3 == 1 && l5 == 1) || (l5 == 1 && l2 == 1 && l3 == 1))
+            {
+                creditMoney = creditMoney + betMoney * 3 + 25;
+                this.Credits.Text = creditMoney.ToString();
+                this.wins++;
+                this.Win.Text = wins.ToString();
+            }
+            else if ((l1 == l2 && l1 == l3) || (l1 == l2 && l1 == l4) || (l1 == l2 && l1 == l5) || (l1 == l3 && l1 == l4) || (l1 == l3 && l1 == l5) || (l1 == l4 && l1 == l5) || (l2 == l3 && l2 == l4) || (l2 == l3 && l2 == l5) || (l3 == l4 && l3 == l5)) 
+            {
+                creditMoney = creditMoney + betMoney * 3/2 + 5;
+                this.Credits.Text = creditMoney.ToString();
+                this.wins++;
+                this.Win.Text = wins.ToString();
+            }
+            else
+            {              
+                creditMoney = creditMoney - betMoney;
+                this.Credits.Text = creditMoney.ToString();             
+            }
+        }
+        
+        private void Bet_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                this.betMoney = Convert.ToInt32(this.Bet.Text);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
